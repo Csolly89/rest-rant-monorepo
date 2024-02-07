784a0f77086db55c481fed9863d3b2538d0d1b5c
csolly <cjaysolly@gmail.com>
2024-02-07 16:33:43 -0500

    reset back to before session

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -16,26 +16,9 @@ function LoginForm() {
     const [errorMessage, setErrorMessage] = useState(null)
 
     async function handleSubmit(e) {
-        const response = await fetch(`http://localhost:5000/authentication/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(credentials)
-        })
+        e.preventDefault()
 
-        const data = await response.json()
-
-        if (response.status === 200) {
-            setCurrentUser(data.user)
-            history.push(`/`)
-        } else {
-            setErrorMessage(data.message)
-        }
     }
-
-
-
 
     return (
         <main>
